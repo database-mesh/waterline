03e0dbd0d29fa219ed742f7e24fc09fb58387430
mlycore <maxwell92@126.com>
2022-03-22 09:58:41 +0800

    fix: import C for cgo

--- pkg/bpf/load.go
+++ pkg/bpf/load.go
@@ -19,12 +19,14 @@ import (
 	"encoding/binary"
 	"fmt"
 
+	"C"
+	"net"
+	"syscall"
+
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/ringbuf"
 	"github.com/mlycore/log"
 	"golang.org/x/sys/unix"
-	"net"
-	"syscall"
 )
 
 const (
@@ -49,6 +51,7 @@ func (l *Loader) Load(ifaceName string, port uint16) error {
 	}
 
 	return nil
+}
 
 type Objs struct {
 	Prog         *ebpf.Program `ebpf:"sql_filter"`
